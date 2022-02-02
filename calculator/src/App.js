17f1544fd8976d1d79a31b1a32e4e26ceaa7a447
David Pérez Martínez <taborlindp@gmail.com>
2022-02-02 21:03:40 +0100

    simple functionality
    learning how to render components

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button";
+import ButtonBox from "./components/ButtonBox";
+import Screen from "./components/Screen";
+import Wrapper from "./components/Wrapper";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
 }
 
